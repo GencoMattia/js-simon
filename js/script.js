01bce9b996648d3f3b4e35abec3ce56c74ba6fbd
GencoMattia <gencomattia@hotmail.it>
2024-05-21 15:45:55 +0200

    added getRandomNumber function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-const generateButton = document.querySelector("#generate-numbers")
+const generateButton = document.querySelector("#generate-numbers");
+
+generateButton.addEventListener("click", function(){
+
+});
+
+
+// --------------FUNCTIONS------------------
+
+function getRandomNumber(min, max){
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNumber;
+};
