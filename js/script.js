c8bd8929984989ae5748de04d8c8ca781e938b3a
GencoMattia <gencomattia@hotmail.it>
2024-05-21 15:55:27 +0200

    created randomUniqueNumber function

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ function getRandomNumber(min, max){
     const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     return randomNumber;
 };
+
+function getUniqueRandomNumber(blackListedNumber, min, max) {
+    let randomNumber;
+    let isFound = false;
+    while(!isFound){
+        randomNumber = getRandomNumber(min, max);
+        if(!blackListedNumber.includes(randomNumber)){
+            isFound = true;
+        };
+    };
+    return randomNumber
+};
