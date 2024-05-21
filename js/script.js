821d3eb07f164e1c7c618db6e2c67592f4db20a3
GencoMattia <gencomattia@hotmail.it>
2024-05-21 16:04:25 +0200

    added randomNumberArray in eventlistener and for to generate 5 uniquerandomnumbers to push into array

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 const generateButton = document.querySelector("#generate-numbers");
 
 generateButton.addEventListener("click", function(){
+    const randomNumberArray = [];
 
+    for(let i = 0; i < 5; i++){
+        randomNumberArray.push(getUniqueRandomNumber(randomNumberArray, 1, 20));
+    }
+
+    console.log(randomNumberArray);
 });
 
 
 // --------------FUNCTIONS------------------
 
+/**
+ * 
+ * @param {*} min 
+ * @param {*} max 
+ * @returns a random number given a min and a max number
+ */
 function getRandomNumber(min, max){
     const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     return randomNumber;
 };
 
+/**
+ * 
+ * @param {*} blackListedNumber 
+ * @param {*} min 
+ * @param {*} max 
+ * @returns give a random unique number given a min and max numbers
+ */
 function getUniqueRandomNumber(blackListedNumber, min, max) {
     let randomNumber;
     let isFound = false;
